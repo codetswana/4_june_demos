e4571d51ef6c41913ffed5a9910e1ee8ece9d5b4
Razzi Abuissa <razzi53@gmail.com>
2014-06-17 16:11:40 +0200

    add error messages

--- twilio_call.py
+++ twilio_call.py
@@ -1,12 +1,28 @@
 from os import environ
 
-from twilio.rest import TwilioRestClient
+try:
+    from twilio.rest import TwilioRestClient
+except ImportError:
+    print("You need to install Twilio: sudo pip install twilio")
+    exit(0)
 
 
-client = TwilioRestClient(
-    environ['TWILIO_ACCOUNT_SID'],
-    environ['TWILIO_AUTH_TOKEN']
-)
+# In order to use this script, you need to acquire these credentials
+# from twilio.com
+
+# then enter them in your terminal using the following commands:
+
+# export TWILIO_ACCOUNT_SID="XXXXXXXXXX"
+# export TWILIO_AUTH_TOKEN="XXXXXXXXXXX"
+
+try:
+    client = TwilioRestClient(
+        environ['TWILIO_ACCOUNT_SID'],
+        environ['TWILIO_AUTH_TOKEN']
+    )
+except KeyError:
+    "You need to set your Twilio credentials. See https://github.com/twilio/twilio-python"
+    exit(1)
 
 phone_numbers = [
     '+26772986824',
